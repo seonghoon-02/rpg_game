57f4feef09e845ab2b9fcfc667a5ed409aa46302
seonghoon-02 <tutle02@gmail.com>
2024-11-04 15:41:31 +0900

    feat : 캐릭터 정의 클래스 추가

--- bin/rpg_game.dart
+++ bin/rpg_game.dart
@@ -1,5 +1,31 @@
-import 'package:rpg_game/rpg_game.dart' as rpg_game;
+void main() {
 
-void main(List<String> arguments) {
-  print('Hello world: ${rpg_game.calculate()}!');
+}
+
+//게임을 정의하기 위한 클래스
+class Game{
+  //게임을 시작하는 메서드
+  startGame(){
+
+  }
+
+  //전투를 진행하는 메서드
+  battle(){
+
+  }
+
+  //랜덤으로 몬스터를 불러오는 메서드
+  getRandomMonster(){
+
+  }
+}
+
+//캐릭터를 정의하기 위한 클래스
+class Character {
+  String cName;  //캐릭터 이름
+  int cStamina;  //캐릭터 체력
+  int cAttack;   //캐릭터 공격력
+  int cDefense;  //캐릭터 방어력
+
+  Character(this.cName, this.cStamina, this.cAttack, this.cDefense);
 }
